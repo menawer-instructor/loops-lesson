4f7d7a8563e4993545daaa4af9c9a6c4d4eef624
tahanifayez <144371267+tahanifayez@users.noreply.github.com>
2023-09-13 17:02:37 +0300

    tahani changes

--- tahani.js
+++ tahani.js
@@ -5,6 +5,11 @@ console.log(`Hello everyone`)
 
 
 function Yourage(age){
-age=currage-birth
+let birth=1930
+let curryear=2023;
+age=curryear-birth
+
 console.log(`my age is ${age}`)
 }// end yourage 
+
+Yourage(1930)
