f16dc07767bbea981bbb7d5d302c770def96c946
Menawer <menawer.work@gmail.com>
2023-09-14 16:39:41 +0300

    adding coffeeshop folder

---
+++ coffeshop/index.js
@@ -0,0 +1,21 @@
+//Code By Menawer, Takes input from user
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+//This Takes
+async function takeInput(question) {
+  return await new Promise((resolve) => {
+    readline.question(question, resolve);
+  });
+}
+//Dont look at above code yet.
+
+//This Runs the shop
+async function runShop() {
+  const answer = await takeInput("How Many Cookies?");
+}
+
+//This is Last line
+runShop();
