989d3db3ed8061fbf9efba398cc0924b017e7e2f
ghizlane23 <ghiza.essaidi@gmail.com>
2023-09-14 18:04:49 +0300

    ghizlane

--- coffeshop/index.js
+++ coffeshop/index.js
@@ -46,7 +46,8 @@ async function runShop() {
 
   myFatoora(cookies, coffees, cookiesCost, cofeecost, totalcost, name);
   if (totalcost >= 3) {
-    let nextCoupon = `${name}" "${getRandomInt(1, 100)}`;
+    let intRandom = Math.floor(Math.random() * 100) + 1;
+    let nextCoupon = `${name}${intRandom}}`;
     console.log(
       `Congratulation you get a promotion coupon ${nextCoupon} for your next order`
     );
