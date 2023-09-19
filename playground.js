57138307dbc6d67202084df688ce6cd200e33fc4
ahmed-hasan92 <ahmed201130030@gmail.com>
2023-09-19 16:55:16 +0300

    Ahmed

---
+++ playground.js
@@ -0,0 +1,19 @@
+//Ahmed
+/**********
+  Question 5: 🌶️🌶️
+  listAllCourses(students):
+  - recieves an array of student objects
+  - returns an array of all unique course names across all students
+	===
+	ANSWER: 
+	[
+	  'Math',    'Science',
+	  'History', 'English',
+	  'Art',     'Music',
+	  'PE'
+	]
+  **********/
+function listAllCourses(students) {
+  // Your code here
+}
+// console.log(listAllCourses(students));
