eb5198129c422545d0ea8f191721cb94584bb13a
Menawer <menawer.work@gmail.com>
2023-09-19 19:11:06 +0300

    new challenges to array

--- challenges/arrays.js
+++ challenges/arrays.js
@@ -52,3 +52,23 @@ function filterByRole(role){
     // Code here
 }
 
+// Write a function to return the number of users
+// Example: numberOfUsers()
+// Returns: 4
+function numberOfUsers(){
+    // Code here
+}
+
+// Write a function to return the number of users by Role
+// Example: numberOfUsersByRole("Admin")
+// Returns: 2
+function numberOfUsersByRole(role){
+    // Code here
+}
+
+// Write a function to return the name of all users in a new array
+// Example: nameOfUsers()
+// Returns: ["Salwa","Salem",...etc] 
+function nameOfUsers(){
+    // Code here
+}
