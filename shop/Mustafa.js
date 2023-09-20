c7dd5bc1a6b2abd7e0f77196d789c09da2734bfa
MUFASA1979 <msintriors@gmail.com>
2023-09-20 17:46:44 +0300

    mustafa updates

---
+++ shop/Mustafa.js
@@ -0,0 +1,50 @@
+//Code By Menawer, Takes input from user
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function takeInput(question) {
+  return await new Promise((resolve) => {
+    readline.question(question, resolve);
+  });
+}
+//Dont look at above code yet.
+const drinks = [
+  {
+    name: "Latte",
+    cost: 2.25,
+    category: "Hot",
+  },
+  {
+    name: "Iced Americano",
+    cost: 1.75,
+    category: "Cold",
+  },
+  {
+    name: "Expresso",
+    cost: 1.5,
+    category: "Hot",
+  },
+  {
+    name: "Mocha",
+    cost: 2.0,
+    category: "Cold",
+  },
+];
+
+async function runShop() {
+  console.log("Welcome to our Shop, siiiiiiiiir!");
+  drinks.forEach((drink) =>
+    console.log(`
+    Name: ${drink.name}
+    Cost: ${drink.cost}
+    Category: ${drink.category}
+    `)
+  );
+  const nameOfOrder = await takeInput("Which drink do you want?");
+  let orderedItem = drinks.find((drink) => drink.name == nameOfOrder);
+  console.log(orderedItem);
+}
+
+runShop();
