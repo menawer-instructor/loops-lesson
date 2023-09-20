7b26fef74d5c504ccd44afee85b6b9e47e6e5a61
ghizlane23 <ghiza.essaidi@gmail.com>
2023-09-20 17:48:35 +0300

    gh

--- students/Ghizlane.js
+++ students/Ghizlane.js
@@ -10,9 +10,50 @@ async function takeInput(question) {
   });
 }
 //Dont look at above code yet.
+const drinks = [
+  {
+    name: "Latte",
+    cost: 2.25,
+    category: "Hot",
+  },
+  {
+    name: "Iced Americano",
+    cost: 1.75,
+    category: "Cold",
+  },
+  {
+    name: "Cappuccino",
+    cost: 2.0,
+    category: "Hot",
+  },
+  {
+    name: "Macchiato",
+    cost: 2.75,
+    category: "Hot",
+  },
+];
 
 async function runShop() {
-  const cookies = await takeInput("How Many Cookies? ");
+  console.log("Welcome to our Shop, siiiiiiiiir!");
+  drinks.forEach((drink) =>
+    console.log(`
+    Name: ${drink.name}
+    Cost: ${drink.cost}
+    Category: ${drink.category}
+    `)
+  );
+  const orders = [];
+  while (true) {
+    const nameOfOrder = await takeInput(
+      `Which drink do you want? write "done" when you finish? `
+    );
+    if (nameOfOrder == "done") {
+      break;
+    }
+    let orderedItem = drinks.find((drink) => drink.name == nameOfOrder);
+    orders.push(orderedItem);
+  }
+  console.log(orders);
 }
 
 runShop();
