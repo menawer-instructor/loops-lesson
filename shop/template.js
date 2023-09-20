aacca23ced114a269d3c43b25e8f16e4a436a95c
ghizlane23 <ghiza.essaidi@gmail.com>
2023-09-20 17:23:15 +0300

    ghiz

--- shop/template.js
+++ shop/template.js
@@ -42,7 +42,7 @@ async function runShop() {
   Category: ${drink.category}
   `)
   );
-  const nameOfOrder = await takeInput("Which drink do you want?");
+  const nameOfOrder = await takeInput("Which drink do you want? ");
   let orderedItem = drinks.find((drink) => drink.name == nameOfOrder);
   console.log(orderedItem);
 }
