bebd60e0eb0a0d0674c42f2cb3e4a9ff283666ba
fatma <fatmaalhardan66@gmail.com>
2023-09-20 18:32:18 +0300

    done

---
+++ shop/fatmamap.js
@@ -0,0 +1,7 @@
+let fatmasNumbers = [10, 20, 30, 40];
+fatmasNumbers = fatmasNumbers.map((el) => el + 2);
+console.log(fatmasNumbers);
+
+let fatmafriend = ["manor", "noor", "fatma", "rayan"];
+//console.log(fatmafriend[0].toUpperCase());
+fatmafriend.forEach((i) => console.log(i.toUpperCase()));
