2a1aa31da758bd2e94cd49cf271d7a39b082db64
ghizlane23 <ghiza.essaidi@gmail.com>
2023-09-20 17:11:14 +0300

    ghizlane

---
+++ students/Ghizlane.js
@@ -0,0 +1,18 @@
+//Code By Menawer, Takes input from user
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function takeInput(question) {
+  return await new Promise((resolve) => {
+    readline.question(question, resolve);
+  });
+}
+//Dont look at above code yet.
+
+async function runShop() {
+  const cookies = await takeInput("How Many Cookies? ");
+}
+
+runShop();
